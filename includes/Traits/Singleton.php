203547c21dd3ef266b0df9e9bef899c3a59b142b
mehdi1413 <mehdifani4@gmail.com>
2026-06-18 16:45:29 +0330

    add singleton trait

--- includes/Traits/Singleton.php
+++ includes/Traits/Singleton.php
@@ -1,7 +1,45 @@
 <?php
+/**
+ * Traits: Singleton
+ *
+ * @package Themora
+ */
 
 namespace Themora\Inc\Traits;
 
-class Singleton {
+use Exception;
 
+defined( 'ABSPATH' ) || exit;
+
+trait Singleton {
+
+	/**
+	 * Singleton Instantiation Method
+	 *
+	 * @return static
+	 */
+	final public static function getInstance(): static {
+		static $instance = [];
+
+		$called_class = static::class;
+
+		if ( ! isset( $instance[ $called_class ] ) ) {
+			$instance[ $called_class ] = new static();
+		}
+
+		return $instance[ $called_class ];
+	}
+
+	private function __clone() {
+	}
+
+	private function __construct() {
+	}
+
+	/**
+	 * @throws Exception
+	 */
+	final public function __wakeup() {
+		throw new Exception( 'Singleton cannot be unserialized' );
+	}
 }
